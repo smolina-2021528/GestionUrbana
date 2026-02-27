b7ac46dd087f21b0012dafff46beb44bbf9834df
soxcal-2021265 <soxcal-2021265@kinal.edu.gt>
2026-02-27 10:46:34 -0600

    feat: Agregar buildHeatmapPoint y HEATMAP_WEIGHTS a geo-helpers

--- utils/geo-helpers.js
+++ utils/geo-helpers.js
@@ -1,4 +1,11 @@
 import { buildReportResponse } from "./report-helpers.js";
+import { PRIORITY_COLORS } from "../helpers/report-constants.js";
+
+export const HEATMAP_WEIGHTS = {
+    ALTA: 1.0,
+    MEDIA: 0.6,
+    BAJA: 0.3,
+};
 
 export const buildGeoPoint = (latitude, longitude) => ({
     type: "Point",
@@ -28,3 +35,14 @@ export const buildReportGeoResponse = (report) => {
         hasLocation,
     };
 };
+
+export const buildHeatmapPoint = (report) => ({
+    id: report.Id,
+    latitude: report.Latitude != null ? parseFloat(report.Latitude) : null,
+    longitude: report.Longitude != null ? parseFloat(report.Longitude) : null,
+    weight: HEATMAP_WEIGHTS[report.Priority] ?? HEATMAP_WEIGHTS.MEDIA,
+    category: report.Category,
+    priority: report.Priority,
+    priorityColor: PRIORITY_COLORS[report.Priority],
+    status: report.Status,
+});
